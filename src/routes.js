d8ccdb13779fd9deeb8b6e0d5e22b8ae6de07182
Neo Sepulveda <israel.sepulveda.castillejo@gmail.com>
2017-03-19 08:24:12 +0000

    Promoted PostsList from component to smart component and wired up the fetch posts action on a react lifecycle function

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 
 import App from './components/app';
-import PostsIndex from './components/posts_index';
+import PostsIndex from './containers/posts_index';
 
 export default (
   <Route path="/" component={App}>
