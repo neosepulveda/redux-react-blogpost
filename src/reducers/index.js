c7b0a137351752d20b35862bb74143a0dcf607e2
Neo Sepulveda <israel.sepulveda.castillejo@gmail.com>
2017-03-19 09:08:15 +0000

    Wired up the reducer of redux-form to the rootReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import PostsReducer from './reducer_posts';
+import  { reducer as formReducer } from 'redux-form';
 
 const rootReducer = combineReducers({
-  posts: PostsReducer
+  posts: PostsReducer,
+  form: formReducer //Make sure that you use form key here and not anything else
 });
 
 export default rootReducer;
