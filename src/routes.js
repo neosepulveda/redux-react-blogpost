3c47961bc28919011db6398e67694bfb8d70671f
Neo Sepulveda <israel.sepulveda.castillejo@gmail.com>
2017-03-19 14:25:59 +0000

    Added a dynamic route and created a component that receives a param

--- src/routes.js
+++ src/routes.js
@@ -4,10 +4,15 @@ import { Route, IndexRoute } from 'react-router';
 import App from './components/app';
 import PostsIndex from './containers/posts_index';
 import PostsNew from './containers/posts_new';
+import PostsShow from './containers/posts_show';
 
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={PostsIndex} />
-    <Route path='posts/new' component={PostsNew} />
+    <Route path="posts/new" component={PostsNew} />
+    <Route path="/posts/:id" component={PostsShow} />
   </Route>
 );
+
+//react-router would pass /post/:id to PostsShow as props.
+//Particulary, this.props.params.id
