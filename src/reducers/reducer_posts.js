97fd4ed5b67fa11c7b3bbd60f801be6094ee6ec2
Neo Sepulveda <israel.sepulveda.castillejo@gmail.com>
2017-03-19 16:11:45 +0000

    Added a new case statement for the reducer. It just returns the data

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,4 +1,4 @@
-import  { FETCH_POSTS, FETCH_POST } from '../actions/index';
+import  { FETCH_POSTS, FETCH_POST, DELETE_POST } from '../actions/index';
 
 const INITIAL_STATE = { all: [], post: null };
 
@@ -8,6 +8,8 @@ export default function (state = INITIAL_STATE, action) {
     return { ...state, post: action.payload.data }
   case FETCH_POSTS:
     return { ...state, all: action.payload.data };
+  case DELETE_POST:
+    return state;
   default:
     return state;
   }
